13226ea7f3ec13c01c06a9b14f5bf1309a850908
GitHubJIYEON <jiyeon2954@gmail.com>
2026-03-12 17:57:58 +0900

    feat: add authentication layout and error handling to router - Introduced AuthLayout and AuthPage components for user authentication. - Added ErrorPage component for error handling in the router. - Updated router configuration to include authentication routes and error handling.

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import AuthLayout from "./features/components/layouts/AuthLayout.tsx";
+import AuthPage from "./pages/auth/AuthPage.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
 
     // auth , error
-    // 관리자 유형, 기관 관리, 조직도 관리, 사용자 관리, 템플릿 관리, 판매관리
+    // 시작하기, 대시보드, 메시지 보내기, 주소록, 발송결과, 발신번호
+    children: [
+      {
+        path: "auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "login",
+            element: <AuthPage />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
